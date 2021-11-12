859221cd7ab3d637313eb829f4923c8177ae3aa7
Diego Inácio <diegodci@gmail.com>
2021-11-12 10:29:10 -0300

    Update assets script

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,34 @@
 // ! Footer
+// * Copyright
 const footer = document.getElementById("footer");
 
-const footer_p = document.createElement("p");
+let footer_p = document.createElement("p");
 footer_p.className = "copyright";
 footer.appendChild(footer_p);
 
 let year_ft = new Date().getFullYear();
 footer_p.innerText = `© ${year_ft} Diego Inácio.`;
+
+// * Links
+let footer_div = document.createElement("div");
+let footer_a = document.createElement("a");
+footer_a.className = "website";
+footer_a.href = "https://diegoinacio.github.io/";
+footer_a.title = "Personal Website";
+footer_a.target = "_blank";
+footer_a.innerText = "diegoinacio.github.io";
+footer_div.appendChild(footer_a);
+footer.appendChild(footer_div);
+
+footer_div = document.createElement("div");
+footer_a = document.createElement("a");
+footer_a.className = "github";
+footer_a.href = "https://github.com/diegoinacio";
+footer_a.title = "GitHub";
+footer_a.target = "_blank";
+footer_a.innerText = "github.com/diegoinacio";
+footer_div.appendChild(footer_a);
+footer.appendChild(footer_div);
 
 // * Easter Egg
 const ee_div = document.createElement("div");
